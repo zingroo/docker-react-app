35df6f133b48e5de974e0a27696aa35523ab79b7
AJ <d.ajinkya16@gmail.com>
2019-03-29 18:15:53 +0530

    branching out

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            General Kenobi.
+            Force is strong with this one.
           </p>
           <a
             className="App-link"
